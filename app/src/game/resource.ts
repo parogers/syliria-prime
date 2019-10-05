f0f7adc5d09188b1e227544a3b267fd3488d49f0
Peter Rogers <peter.rogers@gmail.com>
2019-10-05 19:41:16 -0400

    Added some extra error handling in getTexture

--- app/src/game/resource.ts
+++ app/src/game/resource.ts
@@ -1,14 +1,29 @@
+
+declare var PIXI: any;
+
 export var Resource = {
     CHARS: 'assets/characters.json',
     VEGETATION: 'assets/vegetation.json',
     FOREST: 'assets/forest.png',
+    LETTERS: 'assets/media/letters.json',
 };
 
-export function getTexture(res, name?)
+export function getTexture(resName, spriteName?)
 {
-    if (name !== undefined) {
-        return PIXI.loader.resources[res].textures[name];
+    let res = PIXI.loader.resources[resName];
+    if (!res) {
+        console.log('ERROR: cannot find resource', resName);
+        return null;
     }
-    return PIXI.loader.resources[res].texture;
+
+    if (spriteName !== undefined)
+    {
+        let texture = res.textures[spriteName];
+        if (!texture) {
+            console.log('ERROR: cannot find texture', resName, spriteName);
+        }
+        return texture;
+    }
+    return res.texture;
 }
 
