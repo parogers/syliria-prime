3ddac8e9c9d1d53dc71e5bbfae303fb8659056e0
Peter Rogers <peter.rogers@gmail.com>
2019-10-06 02:15:58 -0400

    Updated loader code to automatically load all listed resources

--- app/src/game/loading.ts
+++ app/src/game/loading.ts
@@ -1,3 +1,21 @@
+/* Syliria Prime
+ * Copyright (C) 2019  Peter Rogers (peter.rogers@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * See LICENSE.txt for the full text of the license.
+ */
 
 import { Resource } from './resource';
 
@@ -14,14 +32,13 @@ export class LoadingScreen
 
     start()
     {
+        // Add a cache buster to the URL to avoid browser caching woes
+        // when resources are updated.
+        // TODO - disable in production, and gear to app version instead
         PIXI.loader.defaultQueryString = 'nocache=' + (new Date()).getTime();
-        PIXI.loader
-            .add(Resource.CHARS)
-            .add(Resource.FOREST)
-            .add(Resource.SWAMP)
-            .add(Resource.DESERT)
-            .add(Resource.VEGETATION)
-            .add(Resource.LETTERS);
+        for (let resName in Resource) {
+            PIXI.loader.add(Resource[resName]);
+        }
 
         PIXI.loader.load(() => {
             console.log('done loading assets', PIXI.loader.resources);
